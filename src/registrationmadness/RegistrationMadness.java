92422907a7704c165972c0f56db68c483e7196b1
Michael McLaughlin <michaelmc@gmail.com>
2013-04-21 14:07:35 -0400

    Built course, began student.

---
+++ src/registrationmadness/RegistrationMadness.java
@@ -0,0 +1,20 @@
+package registrationmadness;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class RegistrationMadness {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+    
+    void register() {
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+    }
+
+}
---
+++ src/registrationmadness/Student.java
@@ -0,0 +1,18 @@
+package registrationmadness;
+
+public class Student implements Runnable {
+    
+    
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    boolean inquire(Course course, boolean morning) {
+        return course.inquire(morning);
+    }
+    
+   
+
+}
